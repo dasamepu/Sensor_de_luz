f94840723db4b360290fb9cff0db6151a08f3caf
dasamepu <87206494+dasamepu@users.noreply.github.com>
2022-03-30 17:41:29 -0500

    Add files via upload

---
+++ banderita.js
@@ -0,0 +1,45 @@
+var cinco = request("johnny-five");
+var circuito = new cinco.Board();
+var bombillo, motorcito, celda;
+var turno = 0;
+
+
+circuito.on("ready", prender);
+
+function prender()
+{
+    var configuracion = {pin:"A0", freq: 50};
+    celda = new cinco.Sensor(configuracion);
+
+    bombillo = new cinco.Led(13);
+    bombillo.on();
+
+    motorcito = new cinco.Servo(9);
+    motorcito.to(0);
+
+    ondear();
+}
+
+function ondear()
+{
+    console.log("luz: " + celda.value);
+    var luz = celda.value;
+    if(luz > 800)
+    {
+       if (turno == 1) 
+       {
+           turno = 0;
+           motorcito.to(70);
+       } 
+       else 
+       {
+           turno = 1;
+           motorcito.to(110);
+       }
+    }
+    else
+    {
+        motorcito.to(30);
+    }
+    setTimeout(ondear, 1000);
+} 
